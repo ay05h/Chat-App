12dc69c59992f570e6f3ad075a7e87275838bad6
ay05h <ayushrai4629@gmail.com>
2025-06-30 14:33:42 +0530

    Add user and message controllers, implement user authentication, and establish MongoDB connection

---
+++ server/controllers/message.controller.js
@@ -0,0 +1,180 @@
+import Message from "../models/message.model.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { ApiError } from "../utils/ApiError.js";
+import mongoose from "mongoose";
+import { uploadToCloudinary } from "../utils/Cloudinary.js";
+import { io, socketToUserMap } from "../index.js";
+import User from "../models/user.model.js";
+const getUsersForCurrentUser = async (req, res) => {
+  try {
+    const userId = req.user?._id;
+    const messagedUsersRaw = await Message.aggregate([
+      {
+        $match: {
+          $or: [
+            { sender: new mongoose.Types.ObjectId(userId) },
+            { receiver: new mongoose.Types.ObjectId(userId) },
+          ],
+        },
+      },
+      {
+        $project: {
+          user: {
+            $cond: [
+              { $eq: ["$sender", new mongoose.Types.ObjectId(userId)] },
+              "$receiver",
+              "$sender",
+            ],
+          },
+        },
+      },
+      {
+        $group: {
+          _id: "$user",
+        },
+      },
+    ]);
+
+    const userIds = messagedUsersRaw.map((u) => u._id);
+
+    const users = await User.find({ _id: { $in: userIds } }).select(
+      "fullName avatar _id"
+    );
+
+    const unseenCounts = {};
+    const countPromises = users.map(async (user) => {
+      const count = await Message.countDocuments({
+        sender: user._id,
+        receiver: userId,
+        isRead: false,
+      });
+      unseenCounts[user._id] = count;
+    });
+    await Promise.all(countPromises);
+    const enrichedUsers = users.map((user) => ({
+      _id: user._id,
+      fullName: user.fullName,
+      avatar: user.avatar,
+      unseenMessages: unseenCounts[user._id] || 0,
+    }));
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          { users: enrichedUsers },
+          "Users fetched successfully"
+        )
+      );
+  } catch (error) {
+    console.log("Error in getUsersForCurrentUser:", error.message);
+    throw new ApiError(500, error.message || "Internal Server Error");
+  }
+};
+
+const getAllMessage = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const currentUserId = req?.user._id;
+    const messages = await Message.find({
+      $or: [
+        { sender: currentUserId, receiver: userId },
+        { sender: userId, receiver: currentUserId },
+      ],
+    }).sort({ createdAt: 1 });
+    await Message.updateMany(
+      { sender: userId, receiver: currentUserId, isRead: false },
+      { $set: { isRead: true } }
+    );
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, messages, "Messages fetched successfully"));
+  } catch (err) {
+    console.error("Error in getAllMessage:", err.message);
+    throw new ApiError(500, err.message || "Internal Server Error");
+  }
+};
+
+const markMessageAsRead = async (req, res) => {
+  try {
+    const { messageId } = req.params;
+    const message = await Message.findOneAndUpdate(
+      { _id: messageId },
+      { $set: { isRead: true } },
+      { new: true }
+    );
+
+    if (!message) {
+      throw new ApiError(404, "Message not found");
+    }
+
+    return res.status(200).json(new ApiResponse(200, "Message marked as read"));
+  } catch (error) {
+    console.error("Error in markMessageAsRead:", error.message);
+    throw new ApiError(500, error.message || "Internal Server Error");
+  }
+};
+
+const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const sender = req.user._id;
+    const receiver = req.params.userId;
+
+    if (!text?.trim() && !image) {
+      throw new ApiError(400, "Message text or image is required");
+    }
+
+    const receiverUser = await User.findById(receiver);
+    if (!receiverUser) {
+      throw new ApiError(404, "Receiver not found");
+    }
+
+    const content = {};
+    if (text?.trim()) {
+      content.text = text.trim();
+    }
+
+    if (image) {
+      try {
+        const imageUpload = await uploadToCloudinary(image);
+        content.image = imageUpload?.secure_url;
+      } catch (error) {
+        throw new ApiError(500, "Failed to upload image");
+      }
+    }
+
+    const messageData = {
+      sender,
+      receiver,
+      content,
+    };
+    const newMessage = await Message.create(messageData);
+    const receiverSocketId = socketToUserMap[receiver.toString()];
+
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+      io.to(receiverSocketId).emit("addToUserList", {
+        _id: sender,
+        fullName: req.user.fullName,
+        profilePic: req.user.profilePic,
+      });
+    }
+
+    return res
+      .status(201)
+      .json(new ApiResponse(201, newMessage, "Message sent successfully"));
+  } catch (error) {
+    console.error("Error in sendMessage:", error.message);
+    throw new ApiError(500, error.message || "Internal Server Error");
+  }
+};
+
+export {
+  getUsersForCurrentUser,
+  getAllMessage,
+  markMessageAsRead,
+  sendMessage,
+};
---
+++ server/controllers/user.controller.js
@@ -0,0 +1,254 @@
+import User from "../models/user.model.js";
+import { uploadToCloudinary } from "../utils/Cloudinary.js";
+import { generateAccessAndRefereshToken } from "../utils/generateToken.js";
+import { ApiError } from "./../utils/ApiError.js";
+import { ApiResponse } from "./../utils/ApiResponse.js";
+import jwt from "jsonwebtoken";
+const options = {
+  httpOnly: true,
+  secure: true,
+};
+
+const signup = async (req, res) => {
+  try {
+    const { email, fullName, password, bio, profilePic } = req.body;
+    if (
+      [email, fullName, password, bio].some((field) => field?.trim() === "")
+    ) {
+      throw new ApiError(400, "All Fields are required.");
+    }
+    const isUserExist = await User.findOne({ email });
+    if (isUserExist) {
+      throw new ApiError(409, "Email already exist !");
+    }
+    let profilePicURL = "";
+    if (profilePic) {
+      const profileResponse = await uploadToCloudinary(profilePic);
+      profilePicURL = profileResponse?.secure_url;
+    }
+
+    const user = await User.create({
+      email,
+      fullName: fullName.trim(),
+      profilePic: profilePicURL,
+      password,
+      bio: bio.trim(),
+    });
+
+    if (!user) {
+      throw new ApiError(
+        500,
+        "Something went wrong while registering the user"
+      );
+    }
+
+    const userObj = user.toObject();
+    delete userObj.password;
+
+    return res
+      .status(201)
+      .json(
+        new ApiResponse(
+          201,
+          { user: userObj },
+          "Account created! Please log in. "
+        )
+      );
+  } catch (err) {
+    console.log(err.message);
+    throw new ApiError(500, err.message);
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const email = req.body.email?.trim();
+    const password = req.body.password?.trim();
+
+    if (!email || !password) {
+      throw new ApiError(400, "Email and password are required");
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user || !(await user.isPasswordCorrect(password))) {
+      throw new ApiError(401, "Invalid credentials!");
+    }
+
+    const { accessToken, refreshToken } = await generateAccessAndRefereshToken(
+      user._id
+    );
+
+    const userObj = user.toObject();
+    delete userObj.password;
+
+    return res
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", refreshToken, options)
+      .json(
+        new ApiResponse(
+          200,
+          {
+            user: userObj,
+            accessToken,
+            refreshToken,
+          },
+          "Login successful!"
+        )
+      );
+  } catch (err) {
+    console.error("Login error:", err);
+    return res
+      .status(err.statusCode || 500)
+      .json(
+        new ApiResponse(
+          err.statusCode || 500,
+          null,
+          err.message || "Internal Server Error"
+        )
+      );
+  }
+};
+
+const logout = async (req, res) => {
+  const userId = req.user._id;
+  try {
+    await User.findByIdAndUpdate(
+      userId,
+      {
+        $set: {
+          refreshToken: "",
+        },
+      },
+      {
+        new: true,
+      }
+    );
+
+    return res
+      .status(200)
+      .clearCookie("accessToken", options)
+      .clearCookie("refreshToken", options)
+      .json(new ApiResponse(200, { data: "hello" }, "Logout Successfull"));
+  } catch (err) {
+    console.error("Logout Error:", err);
+    throw new ApiError(500, "Error while logging out");
+  }
+};
+
+const refreshAccessToken = async (req, res) => {
+  const inComingRefreshToken =
+    req.cookies.refreshToken || req.body.refreshToken;
+
+  if (!inComingRefreshToken) {
+    throw new ApiError(401, "Unauthorized request");
+  }
+
+  try {
+    const decodedToken = jwt.verify(
+      inComingRefreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    );
+
+    const user = await User.findById(decodedToken._id);
+    if (!user) {
+      throw new ApiError(401, "Invalid refresh token");
+    }
+
+    if (inComingRefreshToken !== user.refreshToken) {
+      throw new ApiError(401, "Refresh token is expired or used!");
+    }
+
+    const { accessToken, newRefreshToken } =
+      await generateAccessAndRefereshToken(user._id); // updates token in DB
+
+    return res
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", newRefreshToken, options)
+      .json(
+        new ApiResponse(
+          200,
+          { accessToken, refreshToken: newRefreshToken },
+          "Access token refreshed"
+        )
+      );
+  } catch (err) {
+    throw new ApiError(401, err?.message || "Invalid refresh token");
+  }
+};
+
+const checkAuth = (req, res) => {
+  res.status(200).json(new ApiResponse(200, { user: req.user }, "User Auth"));
+};
+
+const updateProfile = async (req, res) => {
+  const bio = req.body.bio?.trim();
+  const fullName = req.body.fullName?.trim();
+  const profilePic = req.body.profilePic?.trim();
+
+  if (!bio || !fullName) {
+    throw new ApiError(400, "Full name and bio are required");
+  }
+
+  try {
+    let updateFields = { bio, fullName };
+
+    if (profilePic) {
+      const profile = await uploadToCloudinary(profilePic);
+      if (!profile?.secure_url) {
+        throw new ApiError(500, "Profile image upload failed");
+      }
+      updateFields.profilePic = profile.secure_url;
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.user?._id,
+      { $set: updateFields },
+      { new: true }
+    ).select("-password -refreshToken");
+
+    res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          { user: updatedUser },
+          "Profile updated successfully"
+        )
+      );
+  } catch (err) {
+    console.error("Error updating profile:", err);
+    throw new ApiError(500, "Error updating profile");
+  }
+};
+
+const searchUsers = async (req, res) => {
+  try {
+    const query = req.query.query?.trim();
+    if (!query) {
+      throw new ApiError(400, "Query required");
+    }
+
+    const users = await User.find({
+      fullName: { $regex: query, $options: "i" },
+      _id: { $ne: req.user._id },
+    }).select("_id fullName avatar");
+
+    res.status(200).json(new ApiResponse(200, { users }, "Users fetched"));
+  } catch (error) {
+    console.error("Search error:", error);
+    throw new ApiError(500, "Server error while searching users ");
+  }
+};
+
+export {
+  signup,
+  login,
+  logout,
+  refreshAccessToken,
+  updateProfile,
+  checkAuth,
+  searchUsers,
+};
